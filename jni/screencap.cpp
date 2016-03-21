b3f195abfbe258ef0b48e2c988af951d742ffa37
chuck <2542903208@qq.com>
2016-03-21 16:09:54 +0800

    optimise

--- jni/screencap.cpp
+++ jni/screencap.cpp
@@ -31,7 +31,7 @@
 using namespace android;
 
 static uint32_t DEFAULT_DISPLAY_ID = ISurfaceComposer::eDisplayIdMain;
-
+static int32_t displayId = DEFAULT_DISPLAY_ID;
 
 static status_t vinfoToPixelFormat(const fb_var_screeninfo& vinfo,
         uint32_t* bytespp, uint32_t* f)
@@ -74,111 +74,39 @@ static SkBitmap::Config flinger2skia(PixelFormat f)
  * Signature: (Ljava/lang/String;)I
  */
 JNIEXPORT jbyteArray
-JNICALL ScreenCap_currentscreen(JNIEnv *env,
-		jclass clazz) {
-
+JNICALL ScreenCap_currentscreen(JNIEnv *env,jclass clazz , jint width , jint height) {
 	ProcessState::self()->startThreadPool();
 
-	int32_t displayId = DEFAULT_DISPLAY_ID;
-
-	//const char* fn = env->GetStringUTFChars(jpath,NULL);
-	//LOGI("=====jpath:%s \n", fn);
-
-	//if (fn == NULL) {
-	//	LOGE("=====path = %s \n =====err: %s \n",fn, strerror(errno));
-	//	return 1;
-	//}
-	//int fd = -1;
-	//fd = open(fn, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	//LOGI("=====after open ,fd:%d \n",fd);
-	//if (fd == -1) {
-	//	LOGE("=====err: %s \n", strerror(errno));
-	//	return 2;
-	//}
-
-	void const* mapbase = MAP_FAILED;
-	ssize_t mapsize = -1;
-
 	void const* base = 0;
-	uint32_t w, s, h, f;
-	size_t size = 0;
 
 	ScreenshotClient screenshot;
 	sp < IBinder > display = SurfaceComposerClient::getBuiltInDisplay(displayId);
-	int updateStatus = screenshot.update(display);
+	if(display == NULL){
+		LOGE("display is null");
+		return NULL;
+	}
+	int updateStatus = screenshot.update(display , width , height);
 	bool isUpdate = updateStatus == NO_ERROR;
 	if(!isUpdate){
 		LOGE("judge isUpdate err: %s , displayId = %d , updateStatus = %d\n",strerror(errno) , displayId , updateStatus);
-	}
-	if(display == NULL){
-		LOGE("display is null");
-	}
-	if (display != NULL && isUpdate) {
-		base = screenshot.getPixels();
-		w = screenshot.getWidth();
-		h = screenshot.getHeight();
-		s = screenshot.getStride();
-		f = screenshot.getFormat();
-		size = screenshot.getSize();
-	} else {
-		const char* fbpath = "/dev/graphics/fb0";
-		int fb = open(fbpath, O_RDONLY);
-		LOGI("=====read framebuffer, fb:%d \n", fb);
-		if (fb >= 0) {
-			struct fb_var_screeninfo vinfo;
-			if (ioctl(fb, FBIOGET_VSCREENINFO, &vinfo) == 0) {
-				uint32_t bytespp;
-				if (vinfoToPixelFormat(vinfo, &bytespp, &f) == NO_ERROR) {
-					size_t offset = (vinfo.xoffset + vinfo.yoffset * vinfo.xres)
-							* bytespp;
-					w = vinfo.xres;
-					h = vinfo.yres;
-					s = vinfo.xres;
-					size = w * h * bytespp;
-					mapsize = offset + size;
-					mapbase = mmap(0, mapsize, PROT_READ, MAP_PRIVATE, fb, 0);
-					if (mapbase != MAP_FAILED) {
-						base = (void const *) ((char const *) mapbase + offset);
-					}
-				}
-			}
-			close(fb);
-		}else{
-			LOGE("=====fb = %d , err: %s \n",fb, strerror(errno));
-			return NULL;
-		}
+		return NULL;
 	}
 
+	base = screenshot.getPixels();
+
 	if (base) {
-		SkBitmap b;
-		b.setConfig(flinger2skia(f), w, h, s * bytesPerPixel(f));
-		b.setPixels((void*) base);
-		SkDynamicMemoryWStream stream;
-		SkImageEncoder::EncodeStream(&stream, b, SkImageEncoder::kPNG_Type,
-				SkImageEncoder::kDefaultQuality);
-		SkData* streamData = stream.copyToData();
-		//write(fd, streamData->data(), streamData->size());
-		if(streamData->data() == NULL){
-			LOGE("ScreenCap , SCREEN DATA IS NULL");
-			return NULL;
-		}
-		char* buf = static_cast<char*>(const_cast<void*>(streamData->data()));
-		int length = streamData->size();
+		char* buf = static_cast<char*>(const_cast<void*>(base));
+		int length = screenshot.getSize();
 		jbyteArray array = env->NewByteArray(length);
 		env->SetByteArrayRegion(array , 0 , length , reinterpret_cast<jbyte*>(buf));
-		streamData->unref();
 		return array;
-	}
-	//close (fd);
-	if (mapbase != MAP_FAILED) {
-		munmap((void *) mapbase, mapsize);
 	}
 	return NULL;
 }
 
 
 static JNINativeMethod methods[] = {
-		{"currentscreen","()[B",(void*)ScreenCap_currentscreen},
+		{"currentscreen","(II)[B",(void*)ScreenCap_currentscreen},
 };
 
 static int registerNativeMethods(JNIEnv* env,const char* classname,JNINativeMethod* gMethods,int numMethods ){
